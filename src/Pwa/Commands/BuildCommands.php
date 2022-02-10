940c42ec5e0ff1f6bf4b638f3673b46cf490672e
Juan Carlos Conde <juancarlosc@onetree.com>
2022-02-10 06:34:55 -0400

    make rsync folder

--- src/Pwa/Commands/BuildCommands.php
+++ src/Pwa/Commands/BuildCommands.php
@@ -2,16 +2,28 @@
 
 namespace Onetree\Pwa\Commands;
 
-use Onetree\Task\Yarn\Tasks;
-//use Robo\Task\Npm\Tasks;
+use Onetree\Task\Yarn\Tasks as YarnTasks;
 use Robo\Symfony\ConsoleIO;
+use Robo\Task\Remote\Tasks as RsyncTasks;
 
 class BuildCommands extends \Robo\Tasks
 {
-    use Tasks;
+    use YarnTasks;
+    use RsyncTasks;
 
     public function build(ConsoleIO $io)
     {
         $this->taskYarnInstall()->run();
+        $this->taskRsync()
+            ->fromPath('lentesplus-static/')
+            ->toHost('localhost')
+            ->toPath('node_modules/@magento/venia-ui/venia-static/')
+            ->recursive()
+            ->wholeFile()
+            ->verbose()
+            ->progress()
+            ->humanReadable()
+            ->stats()
+            ->run();
     }
 }
